f566ef3b593e659dc54fe1c69f0dd0eab678575d
Yu Wang <yuwang95320@gmail.com>
2018-12-20 23:55:54 +0000

    finish wc.c

--- hw0/wc.c
+++ hw0/wc.c
@@ -1,3 +1,84 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <ctype.h>
+
+typedef unsigned long long wc_count_t
+
+wc_count_t tot_line_cnt, tot_word_cnt, tot_char_cnt;
+
+
+void wc(char *);
+void print_wc(wc_count_t, wc_count_t, wc_count_t, char *);
+
 int main(int argc, char *argv[]) {
+    if (argc == 1) {
+        wc(NULL);
+    } else {
+        int tmp = argc;
+        while(argc > 1) {
+            wc(*argv);
+            argv++;
+            argc--;
+        }
+        if (argc > 1) {
+            print_wc(tot_line_cnt, tot_word_cnt, tot_char_cnt, "total");
+        }
+    }
     return 0;
 }
+
+void wc (char* filename) {
+    int fd, len;
+    char* name;
+    char ch;
+    wc_count_t line_cnt, word_cnt, char_cnt;
+    line_cnt = word_cnt = char_cnt = 0;
+
+    if (!file) {
+        fd = STDIN_FILENO;
+        name = "<stdin>";
+    } else {
+        if ((fd = open(filename, O_RDONLY)) < 0) {
+            fprintf(stderr, "wc: %s: No such file or directory\n", file);
+            exit(1)
+        }
+        name = filename
+    }
+
+    while((len = read(fd, &ch, 1)) == 1) {
+        char_cnt++;
+        space = 1;
+        if (isspace(ch)) {
+            if (ch == '\n') {
+                line_cnt++
+            }
+        } else {
+            if (space) {
+                space= 0;
+                word_cnt++;
+            }
+        }
+    }
+
+    tot_char_cnt += char_cnt;
+    tot_word_cnt += word_cnt;
+    tot_line_cnt += line_cnt;
+    print_wc(line_cnt, word_cnt, char_cnt, filename);
+
+    if (close(fd) == -1) {
+        fprintf(stderr, "wc: %s: close file error\n", file);
+        exit(1);
+    }
+}
+
+void print_wc(wc_count_t line_cnt, wc_count_t word_cnt, wc_count_t char_cnt, char* name) {
+    printf("%7ull %7ull %7ull", line_cnt, word_cnt, char_cnt);
+    if (name) {
+        printf(" %s\n", name);
+    } else {
+        printf("\n");
+    }
+}
+
+
