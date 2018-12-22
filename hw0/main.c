cd09f9d67236a3f7e127b59a8a78b48839765e85
Yu Wang <yuwang95320@gmail.com>
2018-12-22 04:43:11 +0000

    finish hw0

--- hw0/main.c
+++ hw0/main.c
@@ -2,9 +2,14 @@
 #include <sys/resource.h>
 
 int main() {
-    struct rlimit lim;
-    printf("stack size: %ld\n", 0L);
-    printf("process limit: %ld\n", 0L);
-    printf("max file descriptors: %ld\n", 0L);
+    struct rlimit slim;
+    struct rlimit plim;
+    struct rlimit flim;
+    getrlimit(RLIMIT_STACK, &slim);
+    getrlimit(RLIMIT_NPROC, &plim);
+    getrlimit(RLIMIT_NOFILE, &flim);
+    printf("stack size: %ld\n", slim.rlim_cur);
+    printf("process limit: %ld\n", plim.rlim_cur);
+    printf("max file descriptors: %ld\n", flim.rlim_cur);
     return 0;
 }
